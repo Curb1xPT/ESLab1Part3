983fb6d6ba2e47cfeef86227e21b540df4662d18
Curb1xPT <75497979+Curb1xPT@users.noreply.github.com>
2025-05-03 14:35:23 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "mbed.h"
+
+#define BLINKING_RATE     300ms
+
+
+int main()
+{
+#ifdef LED1
+    DigitalOut led(LED1);
+    DigitalOut led2(LED2);
+    DigitalOut led3(LED3);
+#else
+    bool led, led2, led3;
+#endif
+
+    while (true) {
+        led = !led;
+        led2 = !led2;
+        led3 = !led3;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
